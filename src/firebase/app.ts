a4a8b6f510977e72fbb291e929f71a6d772da9a3
cinos81 <cinos81@gmail.com>
2023-03-19 13:04:58 +0900

    init

---
+++ src/firebase/app.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1Uz3sY9QgecAtNLop4kyjKvrf1fN9hBE",
+  authDomain: "simple-email-73174.firebaseapp.com",
+  projectId: "simple-email-73174",
+  storageBucket: "simple-email-73174.appspot.com",
+  messagingSenderId: "772727321783",
+  appId: "1:772727321783:web:999fc77d4219a087814932"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
